68089fe55d7ba58d92c349e8a4902cbd06db0369
Tsamrotul Fuad <tsamrotulfuad@live.com>
2025-01-15 06:34:41 +0700

    sorting tahun terbaru

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,12 +10,12 @@ class HomeController extends Controller
 {
     public function index() 
     {
-        $kajianTerbaru = Kajian::latest()->limit(4)->get();
+        $kajianTerbaru = Kajian::latest()->limit(4)->orderBy('tahun', 'desc')->get();
 
         $daftarKajian = Kajian::all();
 
         $tahun = Carbon::now()->isoFormat('Y');
-        // return dd($kajianTerbaru);
+        return dd($kajianTerbaru);
         return view('welcome', compact('kajianTerbaru', 'daftarKajian', 'tahun'));
     }
 
