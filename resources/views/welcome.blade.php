1157e20bf837d005165bb5b49efc5c1731ab71e4
Tsamrotul Fuad <tsamrotulfuad@live.com>
2025-01-15 07:02:28 +0700

    paginate kajian

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -152,6 +152,7 @@
                 </div>
                 @endforeach
             </div>
+            {{ $daftarKajian->links() }}
         </div>
         <div class="container">
             <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
